6970da3af819ed53def2df0a66bb9c0be5785f0d
Luiz Felipe Moureau <luizfelipesmoureau@gmail.com>
2026-05-12 18:07:13 +0000

    Add Basebox deploy script and package scripts

---
+++ deploy.ts
@@ -0,0 +1,18 @@
+import { Basebox } from "@moureau/basebox";
+
+const { BASEBOX_ANON_KEY, BASEBOX_SECRET_KEY } = process.env;
+
+const dist = "./dist/ssg";
+const domain = "moureau.dev";
+const publicKey = BASEBOX_ANON_KEY;
+const apiKey = BASEBOX_SECRET_KEY;
+
+const bb = new Basebox({ publicKey });
+
+const { success } = await bb.managed({ apiKey }).deploy({ dist, domain });
+
+if (success) {
+  console.log(`Deployed successfully at https://${domain}`);
+} else {
+  console.log("Failed to deploy.");
+}
